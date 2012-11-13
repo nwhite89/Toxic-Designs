d154ff771787d40027a7cbe7e0541ad8e009cdef
Nick White <nickwhite@toxicd.co.uk>
2012-11-13 18:01:20 +0000

    xxx

--- application/views/home.php
+++ application/views/home.php
@@ -22,36 +22,14 @@
                     </li>
                 </ul>
             </div>
-            <style>
-            .tweet-card {
-                border-radius: 5px;
-                background: rgb(246,246,246); /* Old browsers */
-                /* IE9 SVG, needs conditional override of 'filter' to 'none' */
-                background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2Y2ZjZmNiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlMWUxZTEiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
-                background: -moz-linear-gradient(top,  rgba(246,246,246,1) 0%, rgba(225,225,225,1) 100%); /* FF3.6+ */
-                background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(246,246,246,1)), color-stop(100%,rgba(225,225,225,1))); /* Chrome,Safari4+ */
-                background: -webkit-linear-gradient(top,  rgba(246,246,246,1) 0%,rgba(225,225,225,1) 100%); /* Chrome10+,Safari5.1+ */
-                background: -o-linear-gradient(top,  rgba(246,246,246,1) 0%,rgba(225,225,225,1) 100%); /* Opera 11.10+ */
-                background: -ms-linear-gradient(top,  rgba(246,246,246,1) 0%,rgba(225,225,225,1) 100%); /* IE10+ */
-                background: linear-gradient(to bottom,  rgba(246,246,246,1) 0%,rgba(225,225,225,1) 100%); /* W3C */
-                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f6f6f6', endColorstr='#e1e1e1',GradientType=0 ); /* IE6-8 */
-                border: 1px solid white;
-                width: 14em;
-                padding: 1em;
-                float: left;
-            }
-            .tweet-card .tweet-link {
-                background: #424242;
-                width: 2em;
-                float: left;
-                height: 100%;
-            }
-            </style>
-            <div class="sixteen columns clearfix">
-                <div class="twee-cont clearfix">
+            <div id="tweets" class="sixteen columns clearfix">
+                <div class="tweet-cont clearfix">
                     <div class="tweet-card">
-                        <div class="tag">
+                        <p class="tag">
                             #nodejs
+                        </p>
+                        <div class="posted clearfix">
+                            <span class="by"><a href="http://twitter.com/toxicd89" target="_blank">@toxicd89</a></span><span class="date">12/03/2012</span>
                         </div>
                         <div class="tweet">
                             Mobile London iOS app development is finally back underway and should hit the App Store in the next coming months!
@@ -63,26 +41,6 @@
                 </div>
             </div>
             <div class="sixteen columns clearfix">
-                <div id="tweet3d">
-                    <ul>
-                        <li>
-                            <p class="hashtag">
-                                #Nodejs
-                            </p>
-                            <p class="tweet">
-                                Mobile London iOS app development is finally back underway and should hit the App Store in the next coming months!
-                            </p>
-                            <p class="footer clearfix">
-                                <span class="date">12/11/2012</span>
-                                <span class="user">@toxicd89</span>
-                            </p>
-                        </li>
-                        <li><span>Block 2</span></li>
-                        <li><span>Block 3</span></li>
-                        <li><span>Block 4</span></li>
-                        <li><span>Block 5</span></li>
-                    </ul>
-                </div>
                 <div id="tweetmobi">
                     <ul>
                         <li>
