33c1855c10c16c01724d72fcb7b5b523a9c4eacb
Nick White <nickwhite@toxicd.co.uk>
2012-11-28 13:17:53 +0000

    add social lables to About me page

--- application/views/about.php
+++ application/views/about.php
@@ -1,24 +1,14 @@
     <div class="eight columns about-section">
         <span class="label about bio">Bio</span>
-        <p id="collapseOne">
+        <p>
             I am a recent graduate of the University of Portsmouth, graduating with a First Class (Hons) 
             in Business Information Systems. I have since started working at a Digital Agency called Content Formula LTD,
             as a Website Developer working mainly on PHP, SharePoint 2007 &amp; 2010 development. My job role also consists
             of working with Project and Account Managers to provide Written Requirements for other developers to use to complete
             build work.
         </p>
-        <span class="label about instagram">My Instagram Photos</span>
-        <div class="instagram-photos">
-            <?php
-            foreach ($instagram as $key => $value) {
-                echo '<div class="photo" style="background-image: url('. $value .')"></div>';
-            }
-            ?>
-        </div>
-    </div>
-    <div class="eight columns about-section">
-        <span class="label about experience">Coding Skills</span>
-        <ul id="collapseThree">
+        <span class="label about skills">Coding Skills</span>
+        <ul>
             <li>HTML5</li>
             <li>CSS3</li>
             <li>Node.js</li>
@@ -32,5 +22,22 @@
             <li>XML</li>
             <li>XSLT</li>
         </ul>
+        <span class="label about social">Social Media</span>
+        <div class="clearfix">
+            <p><span class="socialLabel">Twitter</span>@ToxicD89</p>
+            <p><span class="socialLabel">LinkedIn</span>Nick White</p>
+            <p><span class="socialLabel">Github</span>nwhite89</p>
+            <p><span class="socialLabel">Instagram</span>nwhite89</p>
+        </div>
+    </div>
+    <div class="eight columns about-section">
+        <span class="label about instagram">My Instagram</span>
+        <div class="instagram-photos">
+            <?php
+            foreach ($instagram as $key => $value) {
+                echo '<div class="photo" style="background-image: url('. $value .')"></div>';
+            }
+            ?>
+        </div>
     </div>
 </div><!--#container-->
