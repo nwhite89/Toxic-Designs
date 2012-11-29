14efdb14fd45c822b623d70d2f8fe6cf4d869dd4
Nick White <nickwhite@toxicd.co.uk>
2012-11-29 21:11:58 +0000

    start initial development page look

--- application/views/development.php
+++ application/views/development.php
@@ -1,5 +1,34 @@
+<style>
+.project {
+	width: 200px;
+	height: 200px;
+	margin: auto;
+	border-radius: 200px;
+	background: purple;
+	color: white;
+	text-align: center;
+	line-height: 200px;
+}
+</style>
 <div class="sixteen columns">
-	<p>
-		Title of the project
-	</p>
+	<div class="five columns">
+		<div class="project">
+			Mobile London
+		</div>
+	</div>
+	<div class="five columns">
+		<div class="project">
+			I am Ethicon
+		</div>
+	</div>
+	<div class="five columns">
+		<div class="project">
+			Mobile London iOS
+		</div>
+	</div>
+	<div class="five columns">
+		<div class="project">
+			Other
+		</div>
+	</div>
 </div>
