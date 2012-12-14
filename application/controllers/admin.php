23ead11e105f0c4d6e067c5fad6d3478b388b447
Nick White <nickwhite@toxicd.co.uk>
2012-12-14 13:55:47 +0000

    create edit and add form for Development Projects

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -36,9 +36,15 @@ class Admin extends CI_Controller
 
 	public function development($pid = null) 
 	{
+		$header = array(
+			'page' => 'admin',
+			'class' => 'development',
+			'title' => 'Admin',
+		);
+		$this->load->view('layouts/header', $header);
 		if($pid == null)
 		{
-
+			$this->load->view('admin/devForm');
 		}
 		else 
 		{	
@@ -48,20 +54,25 @@ class Admin extends CI_Controller
 				'project' => $this->development_model->viewProject($pid),
 			);
 
-			$header = array(
-				'page' => 'admin',
-				'class' => 'development',
-				'title' => 'Admin',
-			);
-
-			$this->load->view('layouts/header', $header);
 			$this->load->view('admin/devForm', $data);
-			$this->load->view('layouts/footer');
 		}
+			$this->load->view('layouts/footer');
 	}
 
 	private function loginForm() 
 	{
 		$this->load->view('admin/loginForm');
+	}
+	public function updateProject()
+	{
+		$array = array('id'=>$this->input->post('id'));
+		$this->db->where($array);
+		$this->db->update('td_development', $this->input->post());
+		$this->development($this->input->post('id'));
+	}
+	public function newProject()
+	{
+		$this->db->insert('td_development', $this->input->post());
+		$this->development($this->db->insert_id());
 	}
 }
--- application/views/admin/devForm.php
+++ application/views/admin/devForm.php
@@ -1,34 +1,40 @@
 <?php
-/*
-foreach ($project->result() as $row)
+if(isset($project))
 {
-
-		echo "<span class=\"label full-width\">" . $row->title . "</span>";
-		echo "<img src=\"" . base_url() . $row->image . "\" />";
-	echo "</div>";
-	echo "<div class=\"eight columns upset\">";
-		if ($row->url != '') echo "<a href=\"". $row->url ."\" class=\"label full-width\" target=\"_blank\">View Site</a>";
-		else echo "<span class=\"label\">Intranet Site</a>";
-	echo "</div>";
-	echo "<div class=\"sixteen columns box\">";
-		echo "<p>" . $row->text . "</p>";
-	echo "</div>";
-}
-
-*/
-
-foreach ($project->result() as $i => $x)
-{
-	/*
-	echo "<div class=\"sixteen columns\">";
-		echo "<form id=\"devProject\" method=\"post\" action=\"\">";
-			echo "<label for=\"projectName\">";
-				echo "<input class=\"full-width\" type=\"text\" name=\"projectName\" id=\"projectName\" placeholder=\"Project Name\" value=\"".$row->title."\" />";
-		echo "</form>";
-	echo "</div>";
-	*/
-	foreach ($x as $y) {
-		echo $y;
+	foreach ($project->result() as $i => $x)
+	{
+		echo '<div class="sixteen columns">';
+			echo '<form id="devProject" method="post" action="'.base_url().'admin/updateProject">';
+				foreach ($x as $f => $y)
+				{
+					if ($f == 'id') { echo "<input class='full-width' type='hidden' name='" . $f . "' id='". $f ."' placeholder='".$f."' value='".$y."'/>"; }
+					else {
+						echo "<label for='". $f ."'>";
+							if ($f !='text') { echo "<input class='full-width' type='text' name='" . $f . "' id='". $f ."' placeholder='".$f."' value='".$y."'/>"; }
+							else { echo "<textarea class='full-width' name='" . $f . "' id='". $f ."' placeholder='".$f."'>".$y."</textarea>"; } 
+						echo "</label>";
+					}
+				}
+				echo '<button type="submit">Submit</button>';
+			echo '</form>';
+		echo '</div>';
 	}
 }
+else 
+{
+	?>
+	<div class="sixteen columns">
+		<form action="<?php echo base_url() ?>admin/newProject" id="devProject" method="post">
+			<label for="title"><input type="text" class="full-width" name="title" id="title" placeholder="Title of Project"></label>
+			<label for="thumbnail"><input type="text" class="full-width" name="thumbnail" id="thumbnail" placeholder="Thumbnail URL"></label>
+			<label for="image"><input type="text" class="full-width" name="image" id="image" placeholder="Image URL"></label>
+			<label for="date"><input type="text" class="full-width" name="date" id="date" placeholder="Date of Project"></label>
+			<label for="text"><textarea type="text" class="full-width" name="text" id="text" placeholder="Text of project"></textarea></label>
+			<label for="url"><input type="text" class="full-width" name="url" id="url" placeholder="URL of site"></label>
+			<button type="submit">Submit</button>
+		</form>
+	</div>
+	<?php
+}
+
 ?>
