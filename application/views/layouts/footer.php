e97b794890b1a195c5cce22140096c9f4af5c0eb
Nick White <nickwhite@toxicd.co.uk>
2013-02-02 12:15:28 +0000

    remove javascript file no longer needed

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -3,7 +3,6 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo base_url(); ?>js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
         <script src="<?php echo base_url(); ?>js/plugins.js"></script>
-        <script src="<?php echo base_url(); ?>js/main.js"></script>
         <script src="<?php echo base_url(); ?>js/bootstrap.js"></script>
         <script src="<?php echo base_url(); ?>js/jquery.roundabout.min.js"></script>
         <script src="<?php echo base_url(); ?>js/easing.js"></script>
