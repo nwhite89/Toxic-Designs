343ccbac3230486642e719a3f6437a605876f454
Nick White <nickwhite@toxicd.co.uk>
2013-02-13 11:51:19 +0000

    sss

--- application/views/labProject.php
+++ application/views/labProject.php
@@ -5,8 +5,7 @@ foreach ($project->result() as $row) {
 		echo "<span class=\"label full-width\">" . $row->title . "</span>";
 	echo "</div>";
 	echo "<div class=\"eight columns upset\">";
-		if ($row->url != '') echo "<a href=\"". $row->url ."\" class=\"label full-width\" target=\"_blank\">View Site</a>";
-		else echo "<span class=\"label\">Intranet Site</a>";
+		echo "<a href=\"". $row->url ."\" class=\"label full-width\" target=\"_blank\">".$row->url_title."</a>";
 	echo "</div>";
 	echo "<div class=\"sixteen columns box\">";
 		echo "<p>" . $row->text . "</p>";
