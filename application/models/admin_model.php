d53dc9b4107aad0d0229133669618894bd1edcf6
Nick White <nickwhite@toxicd.co.uk>
2013-02-22 12:15:46 +0000

    check initalize of admin controller

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -1,11 +1,10 @@
 <?php
-class About_model extends CI_Model
+class Admin_model extends CI_Model
 {
 	public function __construct() 
 	{
 		parent::__construct();
 	}
-
 	public static function checkAdmin() {
 		if ($this->session->userdata('username') == 'nwhite89')
 		{
